11251502a33ec6fd70f0691f37b3a46c44ff6051
Zainal Fahrudin <fnzainal@gmail.com>
2018-05-17 14:30:40 +0700

    update latihan

--- src/main/kotlin/oop/Main.kt
+++ src/main/kotlin/oop/Main.kt
@@ -1,22 +1,35 @@
 package oop
 
+import oop.constructor.Mobil2
+import oop.data.Person
+
 /**
  * fnzainal (c) 2018
  * write on 5/15/18 8:58 PM
  */
 
 fun main(args: Array<String>){
-    val mobil = Mobil()
+    val mobil = Mobil("Honda Mobilio",2015)
+    println("mobil : "+mobil.namaMobil)
+    println("start : "+mobil.tahunMobil)
 
-    mobil.tahunMobil
-    println("nama mobil = "+mobil.namaMobil)
-    println("start : "+mobil.hidupkanMesin())
-    println("tahun : "+mobil.tampilkanTahun())
-    println("pin   : "+mobil.tampilkanPin())
+    val pickUp = Mobil("VW Combi",1998)
+    pickUp.tahunMobil // on mobil
+    pickUp.hidupkanMesin() // on kendaraan
+    pickUp.startMesin() // mesin
 
-    val pickUp = PickUp()
-    pickUp.bukaBak()
-    pickUp.tahunMobil
-    pickUp.hidupkanMesin()
-    pickUp.matikanMesin()
+    val tractor = Traktor()
+    tractor.statBajak()
+    tractor.startMesin()
+    tractor.hidupkanMesin()
+    tractor.matikanMesin()
+
+    val mobil2 = Mobil2("Kijang",2000)
+    println("mobil : "+mobil2.namaMobil)
+    println("start : "+mobil2.tahunMobil)
+
+    val person = Person("Zain",21)
+    print("person is ${person.nama}")
+    print("age is ${person.age}")
+
 }
