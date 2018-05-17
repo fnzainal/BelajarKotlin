347e37bd77f8e511cff631f5c4f96566f35031b4
Zainal Fahrudin <fnzainal@gmail.com>
2018-05-17 14:29:58 +0700

    add class

---
+++ src/main/kotlin/oop/Kendaraan.kt
@@ -0,0 +1,13 @@
+package oop
+
+open class Kendaraan: Mesin() {
+
+    fun matikanMesin(){
+        // function startMesin
+    }
+
+    fun hidupkanMesin(): String {
+        return "gruuumhh.. startMesin dihidupkan."
+    }
+
+}
