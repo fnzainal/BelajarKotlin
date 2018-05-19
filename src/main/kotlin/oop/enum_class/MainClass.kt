f8926bdc7ef15842bec5d8993eec50829bdc9baf
Zainal Fahrudin <fnzainal@gmail.com>
2018-05-19 07:34:27 +0700

    update

--- src/main/kotlin/oop/enum_class/MainClass.kt
+++ src/main/kotlin/oop/enum_class/MainClass.kt
@@ -1,5 +1,7 @@
 package oop.enum_class
 
+import oop.setter_getter.GetterSetterClass
+
 /**
  * fnzainal (c) 2018
  * write on 5/18/18 2:23 PM
@@ -8,10 +10,16 @@ package oop.enum_class
 fun main(args: Array<String>){
     val person: Person = Person()
     println("action: "+person.action)
-    person.doAction(PersonAction.BIKING)
+//    person.doAction(PersonAction.BIKING)
     println("action: "+person.action)
 
     if (person.action.equals(PersonAction.BIKING)){
         println("Wohoo, I'm biking!")
     }
+
+
+    // test setter getter
+    val personx = GetterSetterClass()
+    personx.action=PersonAction.RUN
+    println(personx.action)
 }
